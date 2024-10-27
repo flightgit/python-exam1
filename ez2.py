4121c9cba9d25eb6441fa9ea77ba1ab61c8eccd3
Ron <flightdeckb@gmail.com>
2024-10-27 13:46:08 +0200

    message

---
+++ ez2.py
@@ -0,0 +1,11 @@
+def is_triangular_array(arr):
+    if len(arr) < 3:
+        return False
+    arr.sort()
+    for i in range(len(arr) - 2):
+        if arr[i] + arr[i+1] > arr[i+2]:
+            return True
+    return False
+
+arr = list(map(int, input("Enter Numbers: ").split()))
+print(is_triangular_array(arr))
